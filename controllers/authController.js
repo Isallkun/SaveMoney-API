70771154acd020b92eb3f8c47f958e26509be34f
Faishal AR <Aswadalansori@gmail.com>
2023-12-05 21:03:51 +0700

    added regist

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,17 +1,17 @@
-// authController.js
-
+const bcrypt = require("bcrypt");
 const { db } = require("../config/admin");
-const UserModel = require("../models/userModel"); // Pastikan untuk mengimpor model jika digunakan
+const UserModel = require("../models/userModel");
 
 const AuthController = {
   register: async (req, res) => {
     try {
       const { email, password } = req.body;
-      const userModel = new UserModel(); // Pastikan UserModel diinisialisasi dengan benar
-      const response = await userModel.register(email, password); // Panggil fungsi register dengan email dan password
+      const hashedPassword = await bcrypt.hash(password, 10); // Enkripsi password sebelum registrasi
+      const userModel = new UserModel();
+      const response = await userModel.register(email, hashedPassword); // Gunakan password yang telah dienkripsi
 
       // Jika registrasi berhasil, tambahkan user ke koleksi 'users' di Firestore
-      await db.collection("users").add({ email, password }); // Tambahkan email dan password ke dalam 'users'
+      await db.collection("users").add({ email, password: hashedPassword }); // Tambahkan email dan password yang dienkripsi
 
       res.status(201).json({ message: "User registered successfully", uid: response.uid });
     } catch (error) {
@@ -22,9 +22,15 @@ const AuthController = {
   login: async (req, res) => {
     try {
       const { email, password } = req.body;
-      const userModel = new UserModel(); // Pastikan UserModel diinisialisasi dengan benar
-      const response = await userModel.login(email, password); // Panggil fungsi login dengan email dan password
-      res.status(200).json({ message: "Login successful", uid: response.uid });
+      const userModel = new UserModel();
+      const uid = await userModel.verifyUser(email, password); // Verifikasi password sebelum login
+
+      if (uid) {
+        // Jika verifikasi berhasil, kirimkan respons login sukses
+        res.status(200).json({ message: "Login successful", uid });
+      } else {
+        res.status(401).json({ message: "Login failed", error: "Invalid credentials" });
+      }
     } catch (error) {
       res.status(401).json({ message: "Login failed", error: error.message });
     }
--- models/userModel.js
+++ models/userModel.js
@@ -1,17 +1,44 @@
-// userModel.js
-
-const { admin } = require("../config/admin"); // Pastikan path dan pengambilan objek 'admin' sesuai dengan struktur proyek Anda
+const bcrypt = require("bcrypt");
+const { admin } = require("../config/admin");
 
 class UserModel {
   constructor() {
-    this.auth = admin.auth(); // Mengakses objek 'auth' dari instance Firebase yang diinisialisasi
+    this.auth = admin.auth();
+  }
+
+  async verifyUser(email, password) {
+    try {
+      const userRecord = await this.auth.getUserByEmail(email);
+      const hashedPasswordFromDB = userRecord.customClaims.hashedPassword;
+
+      const isPasswordValid = await bcrypt.compare(password, hashedPasswordFromDB);
+
+      if (isPasswordValid) {
+        return userRecord.uid;
+      } else {
+        throw new Error("Invalid password");
+      }
+    } catch (error) {
+      throw error;
+    }
   }
 
   async register(email, password) {
     try {
-      const userCredential = await this.auth.createUserWithEmailAndPassword(email, password);
-      return { uid: userCredential.user.uid };
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      const userRecord = await this.auth.createUser({
+        email: email,
+        password: hashedPassword,
+        // ... other user information
+      });
+
+      // Set custom claim
+      await this.auth.setCustomUserClaims(userRecord.uid, { hashedPassword });
+
+      return { uid: userRecord.uid };
     } catch (error) {
+      console.error("Error during registration:", error);
       throw error;
     }
   }
@@ -21,6 +48,7 @@ class UserModel {
       const userCredential = await this.auth.signInWithEmailAndPassword(email, password);
       return { uid: userCredential.user.uid };
     } catch (error) {
+      console.error("Error during login:", error);
       throw error;
     }
   }
